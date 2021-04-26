46ebfd1dcc19fd87ad6d9e2cb17296e98249f9ff
soop <>
2021-04-26 23:26:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+04/26/2021
+
+Review
+
+Function    
+
+Recursion  
+- A defined function can call itself. It is a common mathmatical and programming concept.  
+- Every recursive function requires to have a parameter and a base condition.
+
+Formula:
+
+def function(parameter1):
+  if(condition): # base condition
+    return something
+  else:
+    return something + function(parameter2)
+
+
+Where can we apply?
+- Things that have many possible branches and are too complex for an iterative approach. 
+- Recursion works well for searching through types of structure (data structure) because it
+can search multiple branching paths without having many checks/conditions.
+'''
+
